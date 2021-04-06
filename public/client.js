b0785bbc958de360d0cbd677f6b46bf23f2e93f2
Tanjil Rahman <tanjil.rahman01@yahoo.com>
2021-04-06 22:59:56 +0600

    inprogress..

---
+++ public/client.js
@@ -0,0 +1,54 @@
+const publicVapidKey =
+  "BB-z-ha3r_ZYVyY2U-bJCCITqhbZ8_uhKXnURkbT4Q8vh1sU6d87EkKsB2o1wEaf8f_bQLyymNbPqZQAgh83TP8";
+
+// Check for service worker
+// if ("serviceWorker" in navigator) {
+//   send().catch(err => console.error(err));
+// }
+
+// Register SW, Register Push, Send Push
+const send = async() => {
+  // Register Service Worker
+  console.log("Registering service worker...");
+  const register = await navigator.serviceWorker.register("/worker.js", {
+    scope: "/"
+  });
+  console.log("Service Worker Registered...");
+
+  // Register Push
+  console.log("Registering Push...");
+  const subscription = await register.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+  });
+  console.log("Push Registered...");
+
+  // Send Push Notification
+  console.log("Sending Push...");
+  await fetch("/api/subscribe", {
+    method: "POST",
+    body: JSON.stringify(subscription),
+    headers: {
+      "content-type": "application/json"
+    }
+  });
+  console.log("Push Sent...");
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+
+export default send
